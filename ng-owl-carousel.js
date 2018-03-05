170452c5539ca35748778f2de1a9fd432c0430a5
shridharkaushik29 <shridharkaushik29@gmail.com>
2018-03-05 11:28:00 +0530

    Bug Fixes

--- ng-owl-carousel.js
+++ ng-owl-carousel.js
@@ -1,11 +1,7 @@
-angular.module('ngOwlCarousel', [])
+angular.module('owlCarousel', [])
 
-        .directive('owlCarousel', ['$timeout', '$compile', function ($timeout, $compile) {
+        .directive('owlCarousel', ['$timeout', '$compile', '$parse', function ($timeout, $compile, $parse) {
                 return {
-                    scope: {
-                        config: "=",
-                        methods: "=?"
-                    },
                     restrict: 'EA',
                     link: function ($scope, element, attr) {
                         element.hide();
@@ -16,32 +12,85 @@ angular.module('ngOwlCarousel', [])
                             owl = angular.element("<div class='owl-carousel'></div>").insertAfter(element);
                         }
 
-                        $scope.$watch(function () {
-                            return attr.owlId
-                        }, function (value) {
+                        var methods = {
+                            moveTo: function (index, speed) {
+                                owl.trigger('to.owl.carousel', index, speed);
+                                return this;
+                            },
+                            next: function () {
+                                owl.trigger('next.owl.carousel');
+                            },
+                            prev: function () {
+                                owl.trigger('prev.owl.carousel');
+                            },
+                            on: function (event, callback) {
+                                owl.on(event, callback)
+                                return this;
+                            }
+                        }
+
+                        if (attr.owlMethods) {
+                            $parse(attr.owlMethods).assign($scope, methods);
+                        }
+
+                        if (attr.owlInit) {
+                            $scope.$eval(owlInit, {
+                                $owl: owl
+                            })
+                        }
+
+                        $scope.$watch(attr.owlId, function (value) {
                             if (value && value.length) {
-                                owl.attr('id', attr.owlId)
+                                owl.attr('id', value)
                             } else {
                                 owl.removeAttr('id');
                             }
                         })
 
-                        $scope.$watch('config', function (value) {
+                        $scope.$watch(attr.config, function (value) {
                             owl.trigger('destroy.owl.carousel');
-                            owl.owlCarousel(value)
+                            owl.owlCarousel(value);
                         })
 
                         $scope.$watch(function () {
                             return element.html();
                         }, function (value) {
-                            owl.trigger('replace.owl.carousel', value).trigger('refresh.owl.carousel');
+                            _.forEach(element.children(), function (slide, index) {
+                                slide.dataSet = {
+                                    owlIndex: index
+                                }
+                            })
+                            owl.trigger('replace.owl.carousel', [element.children()]).trigger('refresh.owl.carousel').trigger('to.owl.carousel', 0, 0);
                         });
 
-                        $scope.$on('$destroy', function () {
-                            owl.trigger('destroy.owl.carousel');
-                        })
-
+                        if (attr.owlActiveSlide) {
+                            $scope.$watch(attr.owlActiveSlide, function (value) {
+                                if (value !== undefined) {
+                                    methods.moveTo(value)
+                                }
+                            })
+                        }
                     },
                 }
             }
         ])
+        .directive('owlNext', function () {
+            return {
+                restrict: 'AC',
+                link: function ($scope, element, attr) {
+                    element.on('click', function () {
+                        angular.element('#' + attr.owlNext).trigger('next.owl.carousel');
+                    })
+                }
+            }
+        })
+        .directive('owlPrev', function () {
+            return {
+                restrict: 'AC',
+                link: function ($scope, element, attr) {
+                    element.on('click', function () {
+                        angular.element('#' + attr.owlPrev).trigger('prev.owl.carousel');
+                    })
+                }
+            }
+        })
